0b75dfdf40d09ac583381c4c9e5d26df653f0693
Blake Lowrey <blake.lowrey1@gmail.com>
2019-06-05 15:48:32 -0400

    slight update

---
+++ drill3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const totalStupidObj = {
+  dumProperty: 'stoopid',
+  dumLooser: 'dum dum',
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+}
+
+console.log(totalStupidObj.meals[3]);
---
+++ drill4.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const plebArray = [{name: 'Blake', jobTitle: 'Astronaught'}, {name: 'Martha', jobTitle: 'Dog WALKER'},
+  {name: 'Frederick', jobTitle: 'Keiser of Prussia'}];
+
+for (let i = 0; i < plebArray.length; i++){
+  console.log(plebArray[i]);
+}
