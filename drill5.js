c2bc677765965e153d11d5b29eade3126fc1b4f9
Martha Plantz <maplantz89@gmail.com>
2019-06-05 13:09:56 -0700

    finished drill5

---
+++ drill5.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const plebArray = [{name: 'Blake', jobTitle: 'Astronaught', Boss: 'Frederick'}, {name: 'Martha', jobTitle: 'Dog WALKER', Boss: 'Frederick'},
+  {name: 'Frederick', jobTitle: 'Keiser of Prussia'}];
+
+for (let i = 0; i < plebArray.length; i++){
+  if ('Boss' in plebArray[i]){
+    console.log(`${plebArray[i].jobTitle} ${plebArray[i].name} reports to ${plebArray[i].Boss}.`);
+  }else {
+    console.log (`${plebArray[i].jobTitle} ${plebArray[i].name} doesn't report to anybody.`);
+  }
+}
