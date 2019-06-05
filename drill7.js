1fe94dcc07b20b02660bc1826374f8229af09412
Martha Plantz <maplantz89@gmail.com>
2019-06-05 13:59:28 -0700

    first attempt at this factory function

---
+++ drill7.js
@@ -0,0 +1,22 @@
+'use strict';
+const LORTchar = createCharacter();
+let characters = [...createCharacter]
+
+
+function createCharacter(){
+  const char = {
+    name: '', 
+    nickname: '',
+    race: '',
+    origin: '',
+    attack: '',
+    defense: '',
+    describe : function(){
+      return `${this.name} is a ${this.race} from ${this.orgin}`; 
+    },
+    evaluateFight: function(character){
+      return `Your opponent takes ${x} damage and you receive ${y} damage`;
+    }
+  };
+  
+}
