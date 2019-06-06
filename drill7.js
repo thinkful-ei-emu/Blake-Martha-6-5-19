856186885a4f4ea9c5e5d3639b82ec338d2157ab
Blake Lowrey <blake.lowrey1@gmail.com>
2019-06-06 11:54:02 -0400

    new commit

--- drill7.js
+++ drill7.js
@@ -1,25 +1,22 @@
 'use strict';
-const LORTchar = createCharacter();
-let characters = [...createCharacter]
-
 
 function createCharacter(name, nickname, race, origin, attack, defense ){
   return {
-    name : name, 
-    nickname : nickname,
-    race : race,
-    origin : origin,
-    attack : attack,
-    defense : defense,
-    describe : function(){
-      return `${this.name} is a ${this.race} from ${this.orgin}`; 
+    name, 
+    nickname,
+    race,
+    origin,
+    attack,
+    defense,
+    describe(){
+      return `${this.name} is a ${this.race} from ${this.origin}`; 
     },
     evaluateFight: function(character){
       let x = 0;
       let y = 0;
-      if (!(this.defense > character.attack){
-      x = this.attack - character.defense;
-      y = character.attack - this.defense;
+      if (this.defense > character.attack){
+        x = this.attack - character.defense;
+        y = character.attack - this.defense;
       }
       return `Your opponent takes ${x} damage and you receive ${y} damage`;
     }
@@ -27,4 +24,6 @@ function createCharacter(name, nickname, race, origin, attack, defense ){
 }
 let characters = [createCharacter('gandalf the white', 'gandalf', 'Wizard', 'middle Earth', 10, 6 ), 
   createCharacter('bilbo baggins', 'bilbo', 'hobbit', 'shire', 2, 1 ),
-  createCharacter('Legolas', 'glegolas', 'elf', 'neilfheim', 8, 5 ) ]
+  createCharacter('Legolas', 'glegolas', 'elf', 'neilfheim', 8, 5 ) ];
+
+console.log(characters[0].evaluateFight(characters[1]));
